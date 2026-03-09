995c11f4206bf565b32b2b6ef63f4b53a5444ce0
Bruno <161360907+EugeneBruno@users.noreply.github.com>
2026-03-10 00:45:06 +0100

    added comments

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,10 @@
+// here we import the function to createClient from our supabase package 
 import { createClient } from '@supabase/supabase-js';
 
+//we now call the variables/credentials we stored in our env file 
+// ps: without those credentials, our app cannot connect to the database 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
+//we now export the created connection we made with the createClient
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
